a720aa78434b56ec8f8bbe8ab202b21ed3efb453
mapuchila <nyasa98@gmail.com>
2016-12-27 11:46:12 +0300

    Added hello method and template

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
     
 if __name__ == "__main__":
     app.run(debug=True)
